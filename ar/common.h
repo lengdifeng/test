8222cfedee2917140921dcd95185104553dc784a
lengdifeng <904980833@qq.com>
2019-11-26 15:14:30 +0800

    fix ar

---
+++ ar/common.h
@@ -0,0 +1,5 @@
+#ifndef __COMMENT_H__
+#define __COMMENT_H__
+extern int add(int a,int b);
+extern int sub(int a,int b);
+#endif
---
+++ ar/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "common.h"
+int main(int argc,char *argv[])
+{
+	printf("3 + 1 = %d\n", add(3,1));
+	printf("3 - 1 = %d\n", sub(3,1));
+	return 0;
+}
